15bb33fdff462beee2c2b44e156cece310ff7fba
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-10-28 11:17:29 +0300

    Update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const MongoDB = require('./Config/DataBase');
 const mongoose = require("mongoose"); // Data Base Configrations
 const express = require('express');
+const cors = require('cors');
 
 
 // Set Up Port and Make Server listen To requests
@@ -9,6 +10,14 @@ const app = express();
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 app.use(express.json()); // Middleware to parse JSON
+
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
 
 // Users Routes //
 const Trainee_Profile_Routes = require('./Routes/TraineeProfile_Routes');
